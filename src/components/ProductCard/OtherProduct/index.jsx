27fd12b53c3f4f9cae3fedc6360eb39549356a07
Kendzhieva <ahmadiyakendzhieva@gmail.com>
2023-12-11 13:47:37 +0600

    add: OtherProduct

--- src/components/ProductCard/OtherProduct/index.jsx
+++ src/components/ProductCard/OtherProduct/index.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import Good from 'components/Good';
+import styles from './otherProduct.module.css';
 
 function OtherProduct() {
   return (
     <div>
-      OtherProduct
+      <h2 className={styles.title}>Другие товары</h2>
+      <div className={styles.goods}>
+        <Good
+          imgUrl='https://bogatyr.club/uploads/posts/2023-03/1679580679_bogatyr-club-p-stich-na-chernom-fone-vkontakte-27.jpg'
+          name="Гамак “Красный бархат”"
+          text='Описание гамака, основные параметры, материал'
+          price='4 000'
+        />
+
+        <Good
+          imgUrl='https://bogatyr.club/uploads/posts/2023-03/1679580679_bogatyr-club-p-stich-na-chernom-fone-vkontakte-27.jpg'
+          name="Гамак “Красный бархат”"
+          text='Описание гамака, основные параметры, материал'
+          price='4 000'
+        />
+
+        <Good
+          imgUrl='https://bogatyr.club/uploads/posts/2023-03/1679580679_bogatyr-club-p-stich-na-chernom-fone-vkontakte-27.jpg'
+          name="Гамак “Красный бархат”"
+          text='Описание гамака, основные параметры, материал'
+          price='4 000'
+        />
+
+        <Good
+          imgUrl='https://bogatyr.club/uploads/posts/2023-03/1679580679_bogatyr-club-p-stich-na-chernom-fone-vkontakte-27.jpg'
+          name="Гамак “Красный бархат”"
+          text='Описание гамака, основные параметры, материал'
+          price='4 000'
+        />
+      </div>
     </div>
   );
 }
